106dd7051997b5b20b755a4810c56fdd4e3265cb
yugene <yugene1986@gmail.com>
2018-06-23 15:24:40 +0200

    added assertJ, written readme, removed dataprovider, written tests for gist crud and star,

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # GistTests
 
-How to run: gradlew clean test -Dtoken="TOKEN" && gradlew allureReport
+Github gists api tests
+
+How to run: gradlew clean test -Dtoken="GITHUB_TOKEN" && gradlew allureReport
+
+Project created in Kotlin, with gradle, rest assured, testNG, allure report and assertJ
+
+After allureReport will be generated in ./build/reports/allure-report (better open in Firefox)
+Suites has tests results
+
+!(./Suites.bmp)
+
+Timeline shows how it was run in parallel
+
+!(Timeline.bmp)
--- src/test/kotlin/io/fake/GistCRUDTests.kt
+++ src/test/kotlin/io/fake/GistCRUDTests.kt
@@ -1,37 +1,126 @@
 package io.fake
 
 import io.fake.objects.Gist
-import io.restassured.RestAssured.given
+import io.qameta.allure.Issue
+import io.restassured.RestAssured.*
+import org.assertj.core.api.Assertions.assertThat
 import org.testng.annotations.Test
+import java.util.*
 
 class GistCRUDTests {
 
-    @Test
+    @Test()
     fun `Create a gist`() {
-        val gistContent = mapOf("content" to "content")
-        val gist = Gist("descr", true, mapOf("file.txt" to gistContent))
-        given()
+        val gist = newGist()
+        val result = given()
                 .body(gist)
                 .`when`()
                 .post("/gists")
                 .then()
                 .assertThat()
                 .statusCode(201)
+                .and()
+                .extract()
+                .response()
+
+        assertThat(result.jsonPath().getString("description"))
+                .contains(gist.description)
+
+        assertThat(result.jsonPath().getString("files"))
+                .contains(gist.files.keys.first(),
+                        gist.files[gist.files.keys.first()]!!["content"])
+
     }
 
-    @Test
+    @Test()
     fun `Read a gist`() {
+        val gist = newGist()
+        val existingGist = createNewGist(gist)
 
+        val result = get("/gists/$existingGist")
+                .then()
+                .extract()
+                .response()
+
+        assertThat(result.jsonPath().getString("description"))
+                .contains(gist.description)
+        assertThat(result.jsonPath().getString("files"))
+                .contains(gist.files.keys.first(),
+                        gist.files[gist.files.keys.first()]!!["content"])
     }
 
-    @Test
+    @Test()
     fun `Update a gist`() {
+        val oldGist = createNewGist(newGist())
+        val newGist = newGist()
+        val updatedGist = given()
+                .body(newGist)
+                .`when`()
+                .patch("/gists/$oldGist")
+                .then()
+                .extract()
+                .response()
 
+        assertThat(updatedGist.jsonPath().getString("description"))
+                .contains(newGist.description)
+
+        assertThat(updatedGist.jsonPath().getString("files"))
+                .contains(newGist.files.keys.first(),
+                        newGist.files[newGist.files.keys.first()]!!["content"])
     }
 
     @Test
     fun `Delete a gist`() {
+        val gist = createNewGist(newGist())
 
+        delete("/gists/$gist")
+                .then()
+                .assertThat()
+                .statusCode(204)
+        get("/gists/$gist")
+                .then()
+                .assertThat()
+                .statusCode(404)
+    }
+
+    @Test()
+    fun `Check not starred gist`() {
+        val gistId = createNewGist(newGist())
+        get("/gists/$gistId/star")
+                .then()
+                .statusCode(404)
+    }
+
+    @Issue("DEFECT, delete command on non exist resource (star) returns 204 No content, but should return 404")
+    @Test()
+    fun `Edge case unstar not starred gist`() {
+        val gist = newGist()
+        val gistId = createNewGist(gist)
+        given()
+                .body(gist)
+                .delete("/gists/$gistId/star")
+                .then()
+                .statusCode(404)
+    }
+
+    //prerequisite for tests, create a gist and returns its ID
+    private fun createNewGist(gist: Gist): String? {
+        return given()
+                .body(gist)
+                .`when`()
+                .post("/gists")
+                .then()
+                .assertThat()
+                .statusCode(201)
+                .and()
+                .extract().jsonPath().getString("id")
+    }
+
+    //create a new gist object
+    //UUID is needed for making objects unique and keeping tests independent and parallel runnable
+    private fun newGist(): Gist {
+        val gistContent = mapOf("content" to UUID.randomUUID().toString())
+        return Gist(UUID.randomUUID().toString(), true, mapOf(UUID.randomUUID().toString() + ".txt" to gistContent))
     }
 
 }
